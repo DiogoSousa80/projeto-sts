6717faeb4f0713cfa4421a75589132bd84fe0754
DiogoSousa80 <sousadiogo453@gmail.com>
2023-10-31 22:34:35 -0300

    Criação da interface do sistema que faz a convenção do finBy do spring

---
+++ src/main/java/com/meudominio/EmpresaRepository.java
@@ -0,0 +1,9 @@
+package com.meudominio;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, Long> {
+
+    Empresa findByCnpj(String cnpj);
+
+}
